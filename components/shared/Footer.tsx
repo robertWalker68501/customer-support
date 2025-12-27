6078d00205ba6f464da5cd1256671c37fb315620
robert <robert.walker6580@gmail.com>
2025-12-26 16:05:18 -0800

    first commit

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className=''>
+      <h1 className=''>Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/shared/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className=''>
+      <h1 className=''>Header</h1>
+    </div>
+  );
+};
+
+export default Header;
